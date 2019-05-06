acd9ae003ca66a361cf9e1e6388ddd6072e98af5
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 23:00:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves(arr)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.each do |e|
+    e[0].upcase
+    e << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
