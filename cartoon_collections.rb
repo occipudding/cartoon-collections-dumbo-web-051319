ad4f9d8f3027d3d88288ab45d9298ea0ab75c849
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 23:02:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,8 @@ def summon_captain_planet(arr)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? { |e| e.size > 4 }
 end
 
 def find_the_cheese# code an argument here
